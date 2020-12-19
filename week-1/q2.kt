5dc25fca2eefc568feea9f8d57d2c81e16dd199a
anikasharma523 <76254068+anikasharma523@users.noreply.github.com>
2020-12-19 20:43:31 +0530

    Create q2.kt

---
+++ week-1/q2.kt
@@ -0,0 +1,11 @@
+fun main(){
+    val a=2
+    val b=3
+    val c=4
+    if(a>b && a>c)
+    println("$a is the largest number")
+    else if(b>a && b>c)
+    println("$b is the largest number")
+    else
+    println("$c is the largest number")
+}
