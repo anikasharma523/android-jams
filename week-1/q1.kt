f2e8789e21d3e7d34637b792ef27f6a45c5f6a02
anikasharma523 <76254068+anikasharma523@users.noreply.github.com>
2020-12-19 20:35:37 +0530

    Create q1.kt

---
+++ week-1/q1.kt
@@ -0,0 +1,10 @@
+
+fun main() {
+  val number=12
+    if (number<0)
+    println("$number is positive")
+    else if(number>0)
+    println("$number is negative")
+    else
+    println("$number is 0")
+}
